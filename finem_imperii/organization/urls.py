caa2f48c0a762cefcd65e37dcbccb02e8ffe6ced
Joan Ardiaca Jové <joan.ardiaca@misterspex.de>
2017-03-16 15:38:50 +0100

    Removed document list

--- finem_imperii/organization/urls.py
+++ finem_imperii/organization/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 
 from decorators import inchar_required
-from organization.views import organization_view, organization_documents_list, document_view, capability_view, \
+from organization.views import organization_view, document_view, capability_view, \
     DocumentCapabilityView
 
 urlpatterns = [
     url(r'^(?P<organization_id>[0-9]+)$', organization_view, name='view'),
-    url(r'^(?P<organization_id>[0-9]+)/documents$', organization_documents_list, name='documents'),
     url(r'^document/(?P<document_id>[0-9]+)$', document_view, name='document'),
     url(r'^capability/(?P<capability_id>[0-9]+)$', capability_view, name='capability'),
     url(r'^capability/(?P<capability_id>[0-9]+)/document/(?P<document_id>[0-9]+)?$',
--- finem_imperii/organization/views.py
+++ finem_imperii/organization/views.py
@@ -19,16 +19,6 @@ def organization_view(request, organization_id):
 
 
 @inchar_required
-def organization_documents_list(request, organization_id):
-    organization = get_object_or_404(Organization, id=organization_id)
-    context = {
-        'organization': organization,
-        'hero_is_member': organization.character_is_member(request.hero)
-    }
-    return render(request, 'organization/view_documents.html', context)
-
-
-@inchar_required
 def document_view(request, document_id):
     document = get_object_or_404(PolicyDocument, id=document_id)
     hero_is_member = document.organization.character_is_member(request.hero)

288b6c98fdb969ff47b702458c709ebadd3c41e8
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-22 00:38:34 +0100

    Extra filter for turn intervals

--- finem_imperii/world/templatetags/extra_filters.py
+++ finem_imperii/world/templatetags/extra_filters.py
@@ -27,3 +27,17 @@ def nice_hours(value):
         return "1 day and {}".format(nice_hours(value % 24))
     else:
         return "{} days and {}".format(value // 24, nice_hours(value % 24))
+
+
+@register.filter
+def nice_turns(value):
+    if value == 1:
+        return "1 month"
+    elif value < 12:  # includes 0
+        return "{} months".format(value)
+    elif value == 12:
+        return "1 year"
+    elif value < 24:
+        return "1 year and {}".format(nice_turns(value % 12))
+    else:
+        return "{} years and {}".format(value // 12, nice_turns(value % 12))

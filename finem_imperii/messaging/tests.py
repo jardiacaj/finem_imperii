8cc667ea975e1c6c7dc084be74718721bcf83356
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-30 12:53:20 +0200

    Compose and character activation tests

--- finem_imperii/messaging/tests.py
+++ finem_imperii/messaging/tests.py
@@ -1,4 +1,9 @@
+from unittest.case import skip
+
+from django.contrib import auth
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls.base import reverse
 
 from messaging.models import CharacterMessage, MessageRecipientGroup, MessageRecipient
 from organization.models import Organization
@@ -39,3 +44,34 @@ class TestMessageModel(TestCase):
         self.assertEqual(len(post_list), 2)
         self.assertIn("organization_101", post_list)
         self.assertIn("character_3", post_list)
+
+
+class TestCompose(TestCase):
+    fixtures = ['simple_world', 'simple_world_life']
+
+    def setUp(self):
+        self.client.post(
+            reverse('account:login'),
+            {'username': 'alice', 'password': 'test'},
+        )
+        user = auth.get_user(self.client)
+        self.assertEqual(User.objects.get(id=1), user)
+        response = self.client.get(
+            reverse('world:activate_character', kwargs={'char_id':1}),
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.redirect_chain[0][0], reverse('world:character_home'))
+
+    def test_compose_view(self):
+        response = self.client.get(reverse('messaging:compose'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_compose_to_character_view(self):
+        response = self.client.get(
+            reverse('messaging:compose_character', kwargs={'character_id': 2}),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, Character.objects.get(id=2).name)
--- finem_imperii/world/tests.py
+++ finem_imperii/world/tests.py
@@ -1,3 +1,26 @@
+from django.contrib import auth
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls.base import reverse
 
-# Create your tests here.
+
+class TestHome(TestCase):
+    fixtures = ['simple_world', 'simple_world_life']
+
+    def setUp(self):
+        self.client.post(
+            reverse('account:login'),
+            {'username': 'alice', 'password': 'test'},
+        )
+        user = auth.get_user(self.client)
+        self.assertEqual(User.objects.get(id=1), user)
+
+    def test_activate_character(self):
+        response = self.client.get(
+            reverse('world:activate_character', kwargs={'char_id':1}),
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.redirect_chain[0][0], reverse('world:character_home'))

4e22d0b7938c0da36790182174092c62107c24d5
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-30 22:00:04 +0200

    Authenticated homepage test

--- finem_imperii/base/tests.py
+++ finem_imperii/base/tests.py
@@ -4,6 +4,25 @@ from django.urls.base import reverse
 
 
 class HomepageTestCase(TestCase):
-    def test_homepage(self):
+    fixtures = ['simple_world', 'simple_world_life']
+
+    def test_homepage_unauthenticated(self):
         response = self.client.get(reverse('base:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_authenticated(self):
+        self.client.post(
+            reverse('account:login'),
+            {'username': 'alice', 'password': 'test'},
+        )
+        response = self.client.get(reverse('base:home'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.redirect_chain[0][0], reverse('account:home'))
+
+
+class HelpPageTestCase(TestCase):
+    def test_homepage(self):
+        response = self.client.get(reverse('base:help'))
         self.assertEqual(response.status_code, 200)

2611ed878750b3cdc74ea8ba4b9ee52acbcd5eb1
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-30 23:20:03 +0200

    Test get_all_controlled_files

--- finem_imperii/organization/tests.py
+++ finem_imperii/organization/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from organization.models import Organization, Capability
-from world.models import Character
+from world.models import Character, Tile
 
 
 class TestOrganizationModel(TestCase):
@@ -71,25 +71,44 @@ class TestOrganizationModel(TestCase):
         self.assertTrue(kingdom.character_is_member(other_guy))
 
     def test_is_part_of_violence_monopoly(self):
-        kingdom = Organization.objects.get(name="Small Kingdom")
-        self.assertTrue(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="Small Kingdom")
+        self.assertTrue(organization.is_part_of_violence_monopoly())
 
     def test_is_part_of_violence_monopoly2(self):
-        kingdom = Organization.objects.get(name="Small King")
-        self.assertTrue(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="Small King")
+        self.assertTrue(organization.is_part_of_violence_monopoly())
 
     def test_is_part_of_violence_monopoly3(self):
-        kingdom = Organization.objects.get(name="Governor of some plains")
-        self.assertTrue(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="Governor of some plains")
+        self.assertTrue(organization.is_part_of_violence_monopoly())
 
     def test_is_part_of_violence_monopoly4(self):
-        kingdom = Organization.objects.get(name="Helper of the governor of some plains")
-        self.assertTrue(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="Helper of the governor of some plains")
+        self.assertTrue(organization.is_part_of_violence_monopoly())
 
     def test_is_part_of_violence_monopoly5(self):
-        kingdom = Organization.objects.get(name="Small Federation")
-        self.assertFalse(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="Small Federation")
+        self.assertFalse(organization.is_part_of_violence_monopoly())
 
     def test_is_part_of_violence_monopoly6(self):
-        kingdom = Organization.objects.get(name="President of the Small Federation")
-        self.assertFalse(kingdom.is_part_of_violence_monopoly())
+        organization = Organization.objects.get(name="President of the Small Federation")
+        self.assertFalse(organization.is_part_of_violence_monopoly())
+
+    def test_get_all_controlled_tiles(self):
+        organization = Organization.objects.get(name="Small Kingdom")
+        controlled_tiles = organization.get_all_controlled_tiles()
+        self.assertEqual(len(controlled_tiles), 2)
+        self.assertIn(Tile.objects.get(name="Some plains"))
+        self.assertIn(Tile.objects.get(name="Some forest"))
+
+    def test_get_all_controlled_tiles(self):
+        organization = Organization.objects.get(name="Governor of some plains")
+        controlled_tiles = organization.get_all_controlled_tiles()
+        self.assertEqual(len(controlled_tiles), 1)
+        self.assertIn(Tile.objects.get(name="Some plains"))
+
+    def test_get_all_controlled_tiles(self):
+        organization = Organization.objects.get(name="Helper of the governor of some plains")
+        controlled_tiles = organization.get_all_controlled_tiles()
+        self.assertEqual(len(controlled_tiles), 0)
+

07ae38e145ede084681b83b976f8aeac718d0b7e
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-31 14:37:56 +0200

    Fix test

--- finem_imperii/organization/test/test_organization_model.py
+++ finem_imperii/organization/test/test_organization_model.py
@@ -119,7 +119,7 @@ class TestOrganizationModel(TestCase):
     def test_external_capabilities_to_this2(self):
         organization = Organization.objects.get(name="Small Kingdom")
         external_capabilities = organization.external_capabilities_to_this()
-        self.assertEqual(len(external_capabilities), 8)
+        self.assertEqual(len(external_capabilities), 9)
         self.assertTrue(external_capabilities.filter(type=Capability.BAN, organization__name="Small King").exists())
 
     def test_get_position_occupier(self):

0cfec1d837de00a6dfbe960a574079a0cc076818
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-03-31 15:54:36 +0200

    Add codeclimate coverage

--- devops/alpine_tests.sh
+++ devops/alpine_tests.sh
@@ -2,9 +2,9 @@
 set -e
 
 pip3 install coveralls
-#pip install codeclimate-test-reporter
+pip install codeclimate-test-reporter
 apk add --no-cache git
 cd /var/www/finem_imperii/finem_imperii
 coverage run --branch --source . ./manage.py test
 coveralls
-#CODECLIMATE_REPO_TOKEN=<token> codeclimate-test-reporter
+codeclimate-test-reporter

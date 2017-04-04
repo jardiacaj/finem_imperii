aa39ac8846ab1308a15bf96698dcf4d7a56c00f5
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-04-04 16:49:58 +0200

    Fix version setting

--- devops/generate_build_name.sh
+++ devops/generate_build_name.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ "`git tag -l --points-at HEAD`" == "\n" ]; then
+if [ "`git tag -l --points-at HEAD`" == "" ]; then
     git rev-parse HEAD | cut -c -8 > base/templates/base/build.html
 else
     git tag -l --points-at HEAD > base/templates/base/build.html

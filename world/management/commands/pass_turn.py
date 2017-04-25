1759d7450cbfe739ad65679b179cf0414c13a59d
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-04-25 16:07:28 +0200

    Added turn pass management action

---
+++ world/management/commands/pass_turn.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from context_managers import perf_timer
+from world.models import World
+
+
+class Command(BaseCommand):
+    help = 'Passes a turn in the specified world'
+
+    def add_arguments(self, parser):
+        parser.add_argument('world_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        for world_id in options['world_id']:
+            try:
+                world = World.objects.get(pk=world_id)
+            except World.DoesNotExist:
+                raise CommandError('World with id "%s" does not exist' % world_id)
+
+            with perf_timer('World "%s" turn' % world_id):
+                world.pass_turn()
+
+            self.stdout.write(self.style.SUCCESS('Passed turn in world "%s"' % world_id))

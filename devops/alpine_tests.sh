199f9be01bf0067684b6ef25f3581fc7917785c3
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-04-25 20:43:21 +0200

    Fix build

--- devops/alpine_tests.sh
+++ devops/alpine_tests.sh
@@ -6,6 +6,6 @@ pip3 install coveralls
 pip3 install codeclimate-test-reporter
 apk add --no-cache git
 cd /var/www/finem_imperii
-coverage run --branch --source . /usr/bin/python3 ./manage.py test
+coverage run --branch --source . python3 ./manage.py test
 coveralls
 codeclimate-test-reporter

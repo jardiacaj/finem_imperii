3efa852c29bf87be06b18a59ef3bb12cb8404c71
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-04-30 17:30:44 +0200

    Preparations for Postgres

--- finem_imperii/settings.py
+++ finem_imperii/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'HOST': 'localhost',
+        #'NAME': 'fi',
+        #'USER': 'postgres',
+        #'PASSWORD': 'pwd',
+
         #'ENGINE': 'django.db.backends.mysql',
         #'HOST': '/var/run/mysqld/mysqld.sock',
         #'NAME': 'fi',

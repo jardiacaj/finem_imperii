bad5b34f509f49a1222797d4a2050766038d1f31
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-05-07 16:30:26 +0200

    Election tests

--- organization/test/test_elections.py
+++ organization/test/test_elections.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls.base import reverse
 
-from organization.models import Capability, CapabilityProposal, Organization
+from organization.models import Capability, CapabilityProposal, Organization, \
+    PositionElection, PositionCandidacy, CapabilityVote, PositionElectionVote
 
 
 class TestElections(TestCase):
@@ -102,3 +103,127 @@ class TestElections(TestCase):
         self.assertEqual(election.turn, 7)
         self.assertEqual(election.closed, False)
         self.assertEqual(election.winner, None)
+
+        self.assertEqual(PositionCandidacy.objects.exists(), 1)
+        candidacy = PositionCandidacy.objects.get(id=1)
+        self.assertEqual(candidacy.election, self.president.current_election)
+        self.assertEqual(candidacy.candidate_id, self.democracy_leader_id)
+        self.assertEqual(candidacy.description, 'Auto-generated candidacy for incumbent character.')
+        self.assertEqual(candidacy.retired, False)
+
+        response = self.client.get(
+            self.president.current_election.get_absolute_url()
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, candidacy.description)
+
+    def test_present_candidacy(self):
+        self.president.current_election = PositionElection.objects.create(
+            position=self.president,
+            turn=12,
+            closed=False,
+            winner=None
+        )
+        self.president.save()
+
+        response = self.client.post(
+            reverse(
+                'organization:candidacy_capability',
+                kwargs={'capability_id': self.candidacy_capability.id}
+            ),
+            data={'description': 'foobar'},
+            follow=True
+        )
+        self.assertRedirects(
+            response,
+            self.candidacy_capability.get_absolute_url()
+        )
+
+        self.assertEqual(PositionCandidacy.objects.exists(), 1)
+        candidacy = PositionCandidacy.objects.get(id=1)
+        self.assertEqual(candidacy.election, self.president.current_election)
+        self.assertEqual(candidacy.candidate_id, self.democracy_member_id)
+        self.assertEqual(candidacy.description, 'foobar')
+        self.assertEqual(candidacy.retired, False)
+
+        self.assertEqual(PositionElectionVote.objects.count(), 0)
+
+        response = self.client.get(
+            self.president.current_election.get_absolute_url()
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'foobar')
+
+    def test_vote(self):
+        self.president.current_election = PositionElection.objects.create(
+            position=self.president,
+            turn=12,
+            closed=False,
+            winner=None
+        )
+        self.president.save()
+        candidacy = PositionCandidacy.objects.create(
+            election=self.president.current_election,
+            candidate_id=self.democracy_member_id,
+            description='foobar',
+            retired=False
+        )
+
+        response = self.client.get(
+            self.president.current_election.get_absolute_url()
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'foobar')
+
+        response = self.client.get(self.elect_capability.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'foobar')
+
+        response = self.client.post(
+            reverse(
+                'organization:elect_capability',
+                kwargs={'capability_id': self.elect_capability.id}
+            ),
+            data={'candidacy_id': candidacy.id},
+            follow=True
+        )
+        self.assertRedirects(
+            response, self.elect_capability.get_absolute_url())
+
+        self.assertEqual(PositionElectionVote.objects.count(), 1)
+        vote = PositionElectionVote.objects.get(id=1)
+        self.assertEqual(vote.election, self.president.current_election)
+        self.assertEqual(vote.voter_id, self.democracy_member_id)
+        self.assertEqual(vote.candidacy, candidacy)
+
+    def test_pass_election(self):
+        election = PositionElection.objects.create(
+            position=self.president,
+            turn=12,
+            closed=False,
+            winner=None
+        )
+        self.president.current_election = election
+        self.president.save()
+        candidacy = PositionCandidacy.objects.create(
+            election=election,
+            candidate_id=self.democracy_member_id,
+            description='foobar',
+            retired=False
+        )
+        vote = PositionElectionVote.objects.create(
+            election=election,
+            voter_id=self.democracy_member_id,
+            candidacy=candidacy
+        )
+
+        election.resolve()
+        election.refresh_from_db()
+        self.assertEqual(election.closed, True)
+        self.assertEqual(election.winner, candidacy)
+
+        self.president.refresh_from_db()
+        self.assertEqual(
+            self.president.get_position_occupier().id,
+            self.democracy_member_id
+        )

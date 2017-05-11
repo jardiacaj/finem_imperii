cd2b6b6c162ca23334d888dc7a8b9f058fafb1d6
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-05-12 01:15:59 +0200

    Refactor

--- organization/views/capability.py
+++ organization/views/capability.py
@@ -202,6 +202,38 @@ def guilds_settings_view(request, capability_id):
     return capability_success(capability, request)
 
 
+def check_line_formation_validity(request):
+    element_size = int(request.POST['line_depth'])
+    spacing = int(request.POST['line_spacing'])
+    if not 0 < element_size <= 10 or not spacing <= 15:
+        raise Http404("Invalid settings")
+    return element_size, spacing
+
+
+def check_column_formation_validity(request):
+    element_size = int(request.POST['column_width'])
+    spacing = int(request.POST['column_spacing'])
+    if not 0 < element_size <= 10 or not spacing <= 15:
+        raise Http404("Invalid settings")
+    return element_size, spacing
+
+
+def check_square_wedge_iwedge_formation_validity(request):
+    element_size = None
+    spacing = int(request.POST['square_spacing'])
+    if not spacing <= 15:
+        raise Http404("Invalid settings")
+    return element_size, spacing
+
+formation_validity_checks = {
+    BattleFormation.LINE: check_line_formation_validity,
+    BattleFormation.COLUMN: check_column_formation_validity,
+    BattleFormation.SQUARE: check_square_wedge_iwedge_formation_validity,
+    BattleFormation.WEDGE: check_square_wedge_iwedge_formation_validity,
+    BattleFormation.IWEDGE: check_square_wedge_iwedge_formation_validity,
+}
+
+
 @require_POST
 @capability_required_decorator
 def formation_view(request, capability_id):
@@ -209,33 +241,11 @@ def formation_view(request, capability_id):
         Capability, id=capability_id, type=Capability.BATTLE_FORMATION)
 
     new_formation = request.POST['new_formation']
-    if new_formation == BattleFormation.LINE:
-        element_size = int(request.POST['line_depth'])
-        spacing = int(request.POST['line_spacing'])
-        if not 0 < element_size <= 10 or not spacing <= 15:
-            raise Http404("Invalid settings")
-    elif new_formation == BattleFormation.COLUMN:
-        element_size = int(request.POST['column_width'])
-        spacing = int(request.POST['column_spacing'])
-        if not 0 < element_size <= 10 or not spacing <= 15:
-            raise Http404("Invalid settings")
-    elif new_formation == BattleFormation.SQUARE:
-        element_size = None
-        spacing = int(request.POST['square_spacing'])
-        if not spacing <= 15:
-            raise Http404("Invalid settings")
-    elif new_formation == BattleFormation.WEDGE:
-        element_size = None
-        spacing = int(request.POST['wedge_spacing'])
-        if not spacing <= 15:
-            raise Http404("Invalid settings")
-    elif new_formation == BattleFormation.IWEDGE:
-        element_size = None
-        spacing = int(request.POST['iwedge_spacing'])
-        if not spacing <= 15:
-            raise Http404("Invalid settings")
-    else:
+    if new_formation not in formation_validity_checks.keys():
         raise Http404("Invalid formation")
+    else:
+        element_size, spacing = formation_validity_checks[new_formation](
+            request)
 
     proposal = {
         'formation': new_formation,

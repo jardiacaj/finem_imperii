b9922c6b90120dbd7ccef539ca8ab7dd2a968b03
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-05-31 00:12:18 +0200

    Introduce docker logs volume

--- devops/run_container.sh
+++ devops/run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd "$(dirname $0)"
-docker run -v fi:/var/www/finem_imperii/prod -p 8000:80 -d jardiacaj/finem_imperii > live_container
+docker run -v fi:/var/www/finem_imperii/prod -v fi_logs:/var/logs -p 8000:80 -d jardiacaj/finem_imperii > live_container
 popd
 

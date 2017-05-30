4353fb0b3dc0f9ef01909a3b4c39631b68b5ddd9
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-05-31 00:14:45 +0200

    Set log permssions on docker entry

--- devops/alpine_entry.sh
+++ devops/alpine_entry.sh
@@ -2,5 +2,6 @@
 if [ ! -f /var/www/finem_imperii/prod/settings.py ]; then
   cp /var/www/finem_imperii/finem_imperii/settings.py /var/www/finem_imperii/prod/settings.py
 fi
+touch /var/log/django.log && chown apache /var/log/django.log
 tail -F /var/log/apache2/access.log /var/log/apache2/error.log /var/log/django.log &
 httpd -f /etc/apache2/httpd.conf -e info -DFOREGROUND

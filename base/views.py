912f46e0c7e029dd8880ebed37e1b54b02077858
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-05-31 20:25:52 +0200

    Added MOTD to homepage

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 
+from messaging.models import ServerMOTD
+
 
 def homepage(request):
     if request.user.is_authenticated():
         return redirect('account:home')
     else:
-        return render(request, "base/homepage.html")
+        context = {
+            'server_messages': ServerMOTD.objects.all() if request.user.is_staff
+                               else ServerMOTD.objects.filter(draft=False),
+        }
+        return render(request, "base/homepage.html", context=context)

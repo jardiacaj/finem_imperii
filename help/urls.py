391bff55d560210e4e29063e510c09b313bd73b2
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-06-16 20:33:20 +0200

    Help text from final handout

--- help/urls.py
+++ help/urls.py
@@ -4,5 +4,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='help/home.html'), name='home'),
     url(r'^fundamental$', TemplateView.as_view(template_name='help/fundamental.html'), name='fundamental'),
+    url(r'^design', TemplateView.as_view(template_name='help/design.html'), name='design'),
+    url(r'^mechanics', TemplateView.as_view(template_name='help/mechanics.html'), name='mechanics'),
     url(r'^about$', TemplateView.as_view(template_name='help/about.html'), name='about'),
 ]

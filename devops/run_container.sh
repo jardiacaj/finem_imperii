07c4b5fd46af6e4d2764e35a3770528128e252c6
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-06-22 18:30:43 +0200

    Backups before turn pass

--- devops/run_container.sh
+++ devops/run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd "$(dirname $0)"
-docker run -v fi:/var/www/finem_imperii/prod -v fi_logs:/var/logs -p 8000:80 -d jardiacaj/finem_imperii > live_container
+docker run -v fi:/var/www/finem_imperii/prod -v fi_logs:/var/logs -v fi_backups:/backups -p 8000:80 -d jardiacaj/finem_imperii > live_container
 popd
 
--- devops/run_turn.sh
+++ devops/run_turn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pushd "$(dirname $0)"
+./run_command.sh ./manage.py dumpdata > "/backups/$(date --iso-8601=seconds)"
 ./run_command.sh ./manage.py pass_turn $1
 popd

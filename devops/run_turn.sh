540c4f019206f92bf153ea45d3c3810448d4c500
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-06-23 11:34:29 +0200

    Make backups out of container

--- devops/run_turn.sh
+++ devops/run_turn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pushd "$(dirname $0)"
-./run_command.sh ./manage.py dumpdata > "/backups/$(date --iso-8601=seconds)"
+mkdir backups || true
+./run_command.sh ./manage.py dumpdata | gzip > "backups/$(date --iso-8601=seconds).gz"
 ./run_command.sh ./manage.py pass_turn $1
 popd

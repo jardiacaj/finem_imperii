01dd2141563b86b63e7d6e30c2a1ebb9a6cabf1c
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2017-11-19 15:27:10 +0100

    Improved initialization command

--- context_managers.py
+++ context_managers.py
@@ -6,6 +6,7 @@ import time
 @contextmanager
 def perf_timer(name):
     start_time = time.time()
+    print('[{}] ...'.format(name))
     yield
     elapsed = time.time() - start_time
     print('[{}] in {} ms'.format(name, int(elapsed * 1000)))

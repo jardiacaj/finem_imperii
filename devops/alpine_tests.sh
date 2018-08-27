1bb2a300f85dd8309c1ac5673da152f4dfc64412
Joan Ardiaca Jové <joan.ardiaca@misterspex.de>
2018-08-27 11:54:41 +0200

    Added codacy coverage

--- devops/alpine_tests.sh
+++ devops/alpine_tests.sh
@@ -5,3 +5,11 @@ set -e
 cd /var/www/finem_imperii
 apk add --no-cache git
 coverage run --branch --source . ./manage.py test
+
+if [ -z "$CODACY_PROJECT_TOKEN" ]; then
+    echo "Not running codacy reporter (token missing)"
+else
+    coverage xml
+    python-codacy-coverage -r coverage.xml
+fi
+

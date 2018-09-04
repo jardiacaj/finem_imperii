1b8d15f24c1e0995102b7b9bcda6ce4fd4269af9
Joan Ardiaca Jové <joan.ardiaca@misterspex.de>
2018-09-05 01:52:09 +0200

    Some love to character creation and other small changes
    * Improved visuals of character creation process
    * Improved welcome message
    * Added server time to topnav
    * Disabled i18n, l10n, timezones

--- finem_imperii/settings_common.py
+++ finem_imperii/settings_common.py
@@ -105,11 +105,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Berlin'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)

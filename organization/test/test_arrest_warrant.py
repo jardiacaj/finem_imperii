e366543e1ac7c22d6385fa9d3420416ce8dd7eb5
Joan Ardiaca Jové <joan.ardiaca@misterspex.de>
2018-09-10 18:13:27 +0200

    Arrest warrant test

--- organization/test/test_arrest_warrant.py
+++ organization/test/test_arrest_warrant.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.urls.base import reverse
 
 from organization.models.capability import Capability
-from character.models import Character
+from character.models import Character, CharacterEvent
 
 
-class TestBan(TestCase):
+class TestArrestWarrant(TestCase):
     fixtures = ["simple_world"]
 
     def setUp(self):
@@ -18,28 +18,46 @@ class TestBan(TestCase):
             follow=True
         )
 
-    def test_ban(self):
+    def test_warrant(self):
         capability = Capability.objects.get(
             organization_id=102,
-            type=Capability.BAN,
+            type=Capability.ARREST_WARRANT,
             applying_to_id=101
         )
 
-        to_ban = Character.objects.get(id=2)
+        to_arrest = Character.objects.get(id=2)
         response = self.client.get(capability.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "King")
         self.assertContains(response, "Kingdom Member")
 
         response = self.client.post(
-            reverse('organization:banning_capability', kwargs={
+            reverse('organization:arrest_warrant_capability', kwargs={
                 'capability_id': capability.id
             }),
-            data={'character_to_ban_id': to_ban.id},
+            data={'character_to_imprison_id': to_arrest.id},
             follow=True
         )
         self.assertRedirects(
             response, capability.organization.get_absolute_url())
 
-        vm = to_ban.get_violence_monopoly()
-        self.assertEqual(vm.barbaric, True)
+        warrant = CharacterEvent.objects.get(
+            character=to_arrest,
+            active=True,
+            organization_id=101,
+            type=CharacterEvent.ARREST_WARRANT
+        )
+
+        response = self.client.post(
+            reverse('organization:arrest_warrant_revoke_capability', kwargs={
+                'capability_id': capability.id,
+                'warrant_id': warrant.id
+            }),
+            data={},
+            follow=True
+        )
+        self.assertRedirects(
+            response, capability.organization.get_absolute_url())
+
+        warrant.refresh_from_db()
+        self.assertFalse(warrant.active)

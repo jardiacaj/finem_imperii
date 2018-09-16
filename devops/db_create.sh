7402389d9ca40593ba1dd1eca7507bd3e7ec25cc
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2018-09-16 21:26:19 +0200

    Introduce Corelia

--- devops/db_create.sh
+++ devops/db_create.sh
@@ -4,6 +4,6 @@ set -e
 
 pushd "$(dirname $0)/.."
 venv/bin/python ./manage.py migrate
-venv/bin/python ./manage.py loaddata world1 simple_world
-venv/bin/python ./manage.py initialize_world 1 2
+venv/bin/python ./manage.py loaddata world1 simple_world corelia
+venv/bin/python ./manage.py initialize_world 1 2 3
 popd
